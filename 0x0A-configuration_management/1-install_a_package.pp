206497f0b8fa5265060f0acf15de6cd6f7298063
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2024-02-23 14:29:52 -0800

    fix: Adding a command to install Werkzeug package as dependency for flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 # Install flask version 2.1.0
 
-package {'flask':
-  ensure          => '2.1.0',
-  provider        => 'pip3',
-  install_options => ['--no-warn-script-location'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
