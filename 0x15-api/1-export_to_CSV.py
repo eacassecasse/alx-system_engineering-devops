6c39d62d1887faab449952312bec51d829e562cd
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-03-26 01:46:38 +0200

    feat: Modules that consumes a REST API and exports the data to different formats such as CSV and JSON

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Exports the task list information for a given employee ID to CSV format."""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "users/{}".format(employee_id)).json()
+    employee_name = employee.get("username")
+    tasks = requests.get(url + "todos", params={"userId": employee_id}).json()
+
+    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
+        wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [wr.writerow(
+            [employee_id, employee_name, task.get("completed"), task.get("title")]
+         ) for task in tasks]
