870c24fe13c8de82d7669aeb06d622f89f642a95
Edmilson Cassecasse <2015edmilsonb1327@gmail.com>
2024-08-13 11:54:23 +0200

    :sparkles: fix(apache): Fix command in puppet script to address 500 error on WordPress server.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Automated puppet fix (Find out why Apache is returning a 500 error)
 
-exec { 'Fixing a website':
+exec { 'Fixing a wordpress apache server':
   command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
   provider => shell,
 }
