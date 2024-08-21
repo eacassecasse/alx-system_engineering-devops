feb3b6b0ffe337d66700a076dce9f9970780cfa7
Edmilson Cassecasse <2015edmilsonb1327@gmail.com>
2024-08-21 11:54:00 +0200

    fix: Correcting pupet-lint identation issus

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # Use the exec resource to updates the user limits in /etc/security/limits.conf
 
 exec { 'replace-nofile-soft':
-  command  => 'sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  path     => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
-  before   => Exec['replace-nofile-hard'],
+  command => 'sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  before  => Exec['replace-nofile-hard'],
 }
 
 exec { 'replace-nofile-hard':
-  command  => 'sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
-  path     => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  command => 'sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+  path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
 }
