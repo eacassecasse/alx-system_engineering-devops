fbcc33933c51295a73d2192027a033520533451d
Edmilson Cassecasse <2015edmilsonb1327@gmail.com>
2024-08-23 11:45:39 +0200

    fix: Updating the implementation to use provider for shell instead of path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,12 @@
 # Use the exec resource to update or add the ULIMIT variable in /etc/default/nginx
 
-exec { 'replace_ulimit':
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
-  before  => Exec['restart_nginx'],
+exec {'replace_ulimit':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart_nginx'],
 }
 
-exec { 'restart_nginx':
-  command     => 'service nginx restart',
-  path        => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
-  refreshonly => true,  # This ensures that the restart happens only if triggered
+exec {'restart_nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
